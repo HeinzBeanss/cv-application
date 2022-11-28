e0697f1f1810949dac052e6018bcdc3f1d2af0b3
David Bean <heinzbeansss@gmail.com>
2022-11-28 01:02:53 +0000

    Updates

---
+++ precache-manifest.9233a8c34ffe261496a49df1d815323c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7efaa36255577b718d86",
+    "url": "/cv-application/static/css/main.d2f6319a.chunk.css"
+  },
+  {
+    "revision": "7efaa36255577b718d86",
+    "url": "/cv-application/static/js/main.7efaa362.chunk.js"
+  },
+  {
+    "revision": "8d93f9ff4a737ab28f5b",
+    "url": "/cv-application/static/js/1.8d93f9ff.chunk.js"
+  },
+  {
+    "revision": "65881043a4b24edec006",
+    "url": "/cv-application/static/js/runtime~main.65881043.js"
+  },
+  {
+    "revision": "c0e9c3386463ddd17eacb94f98b55605",
+    "url": "/cv-application/index.html"
+  }
+];
